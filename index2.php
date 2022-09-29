90987a3d4a027a70c74a8e2adc46efcfdd6834e9
서용석 <tjdydtjr1@kakao.com>
2022-09-29 17:03:52 +0900

    Button file

---
+++ index2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GMANH</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-white bg-white fixed-top" id="mainNav">
+        <div class="container px-10">
+            <a class="navbar-brand" style="position: absolute;"><a class="btn btn-secondary;" style="width: 200px; height: 80px; font-size: 30px;">GMANH</a></a>
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="navbar-collapse collapse" id="navbarResponsive" style="">
+                <ul class="navbar-nav ms-auto ">
+                
+
+		    <li class="nav-item"><a class="btn btn-primary" href="phpfile/member_form.php">회원가입</a></li>&nbsp;
+
+                    <li class="nav-item"><a class="btn btn-primary" href="phpfile/login_form.php">로그인</a></li>&nbsp;
+		    <li class="nav-item"><a class="btn btn-primary" href="map.php">KaKaoMap</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
